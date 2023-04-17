14cc7d7232d6cc92f8d067fe57a7a8e1dff1828d
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-04-16 21:11:42 -0400

    Update (4/16)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
-
 import { Routes, Route } from 'react-router-dom';
 
-import Spinner from './components/spinner/spinner.component';
-import { checkUserSession } from './store/user/user.action';
-import { GlobalStyle } from './global.styles';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-const Navigation = lazy(() =>
-  import('./routes/navigation/navigation.component')
-);
-const Shop = lazy(() => import('./routes/shop/shop.component'));
-const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
-const Home = lazy(() => import('./routes/home/home.component'));
-const Authentication = lazy(() =>
-  import('./routes/authentication/authentication.component')
-);
+const Shop = () => {
+	return <h1>I am the shop Page</h1>;
+};
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
-
-  return (
-    <Suspense fallback={<Spinner />}>
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
-    </Suspense>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
 
+import './index.scss';
 import App from './App';
-import { store, persistor } from './store/store';
-import { stripePromise } from './utils/stripe/stripe.utils';
-import * as registerServiceWorker from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-
-render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App />
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  rootElement
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
-registerServiceWorker.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
