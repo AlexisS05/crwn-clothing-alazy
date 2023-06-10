c05a2b34f7c00b9df156a5b730ffa1bdd0d0964c
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-06-10 15:57:35 -0400

    Update after learning Jest/Enzyme

---
+++ src/components/button/__tests__/button.component.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../button.component';
+
+describe('button tests', () => {
+	test('should render base button when nothing is passed', () => {});
+});
