dfa1f1ae6ee0dab5897f8bd40fd9254526ec30f1
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2023-06-15 15:43:09 -0400

    quick update for query.

--- src/routes/checkout/checkout.styles.tsx
+++ src/routes/checkout/checkout.styles.tsx
@@ -8,6 +8,10 @@ export const CheckoutContainer = styled.div`
 	align-items: center;
 	margin: 50px auto 0;
 
+	@media screen and (max-width: 800px) {
+		width: 100%;
+	}
+
 	@media screen and (max-width: 400px) {
 		width: 100%;
 	}
